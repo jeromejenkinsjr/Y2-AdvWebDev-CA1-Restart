149184dda43e21ebea1421484fde557f0ea117d8
Jeremi Olczak <n00230483@iadt.ie>
2024-10-29 20:20:03 +0000

    Altering Performances Table migration code

--- 2024_Performance_Hub/database/migrations/2024_10_29_184546_create_performances_table.php
+++ 2024_Performance_Hub/database/migrations/2024_10_29_184546_create_performances_table.php
@@ -10,12 +10,21 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
-        Schema::create('performances', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+{
+    Schema::create('performances', function (Blueprint $table) {
+        $table->id();
+        $table->string('title');
+        $table->string('piece');
+        $table->string('musician');
+        $table->string('event');
+        $table->time('duration');
+        $table->text('description')->nullable();
+        $table->string('composer');
+        $table->string('image')->nullable();
+        $table->timestamps();
+    });
+}
+
 
     /**
      * Reverse the migrations.
