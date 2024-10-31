05ec8b50cff63eeddb1efa62ddfac9edd0ed293f
Jeremi Olczak <n00230483@iadt.ie>
2024-10-31 23:52:13 +0000

    Changing some 'Book' code to 'Performance' and Adding to Performances Index page

--- 2024_Performance_Hub/app/Http/Controllers/PerformanceController.php
+++ 2024_Performance_Hub/app/Http/Controllers/PerformanceController.php
@@ -13,7 +13,7 @@ class PerformanceController extends Controller
      */
     public function index()
     {
-       $performances = Book::all();
+       $performances = Performance::all();
        return view('performances.index', compact('performances'));
     }
 
---
+++ 2024_Performance_Hub/resources/views/performances/index.blade.php
@@ -0,0 +1,29 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('All Performances') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <h3 class="font-semibold text-lg mb-4">List of Performances:</h3>
+                    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+                        @foreach ($performances as $performance)
+                            <x-performance-card
+                                :title="$performance->title"
+                                :piece="$performance->piece"
+                                :author="$performance->author"
+                                :musician="$performance->musician"
+                                :event="$performance->event"
+                                :description="$performance->description"
+                            />
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
