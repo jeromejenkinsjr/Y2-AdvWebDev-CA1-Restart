a89175677bb6f3b10ea9d713cb328063c385dbe7
Jeremi Olczak <n00230483@iadt.ie>
2024-11-01 01:47:00 +0000

    Creating a create.blade.php

--- 2024_Performance_Hub/resources/views/performances/create.blade.php
+++ 2024_Performance_Hub/resources/views/performances/create.blade.php
@@ -1,3 +1,21 @@
-<div>
-    <!-- Waste no more time arguing what a good man should be, be one. - Marcus Aurelius -->
+<x-app-layout>
+<x-slot name="header">
+<h2 class="font-semibold text-xl ☐ text-gray-800 leading-tight">
+{{__('Create New Performance') }}
+</h2>
+</x-slot>
+<div class="py-12">
+<div class="max-w-7x1 mx-auto sm:px-6 1g:px-8">
+<div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+<div class="p-6 ☐ text-gray-900">
+<h3 class="font-semibold text-lg mb-4">Add a New Performance: </h3>
+{{-- Using the PerformanceForm component for performance creation --}}
+<x-book-form
+:action="route('performances.store')"
+:method="'POST'"
+/>
 </div>
+</div>
+</div>
+</div>
+</x-app-layout>
