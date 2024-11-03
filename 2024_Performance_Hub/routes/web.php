a25647ae9a69c1614d17975d67b7bcc233251e74
Jeremi Olczak <n00230483@iadt.ie>
2024-11-03 20:59:17 +0000

    Adding new routes for Edit/Update

--- 2024_Performance_Hub/routes/web.php
+++ 2024_Performance_Hub/routes/web.php
@@ -23,4 +23,9 @@ Route::get('/performances', [PerformanceController::class, 'index'])->name('perf
 Route::get('/performances/create', [PerformanceController::class, 'create'])->name('performances.create');
 Route::get('/performances/{performance}', [PerformanceController::class, 'show'])->name('performances.show');
 Route::post('/performances', [PerformanceController::class, 'store'])->name('performances.store');
+
+// The routes below are for displaying the edit form for a performance. {performance} is a route parameter representing the ID of the performance to be edited. The 'edit' method in PerformanceController is responsible for fetching the specific performance and returning the edit form view.
+
+Route::get('/performances/{performance}/edit', [PerformanceController::class, 'edit'])->name('performances.edit');
+Route::put('/performances/{performance}', [PerformanceController::class, 'update'])->name('performances.update');
 require __DIR__.'/auth.php';
