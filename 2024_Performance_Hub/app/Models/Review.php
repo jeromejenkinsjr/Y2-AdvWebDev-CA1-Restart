c963f38959d7853ac1324420e96562c0bd0ea3e1
Jeremi Olczak <n00230483@iadt.ie>
2024-11-04 12:39:46 +0000

    Updating Review model

--- 2024_Performance_Hub/app/Models/Review.php
+++ 2024_Performance_Hub/app/Models/Review.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    protected $fillable = ['performance_id', 'user_id', 'content', 'rating'];
+
+    // Relationships
+    public function performance()
+    {
+        return $this->belongsTo(Performance::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
