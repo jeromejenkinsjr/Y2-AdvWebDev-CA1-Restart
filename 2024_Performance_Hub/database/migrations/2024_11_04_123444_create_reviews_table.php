cfa634645340dff0a22289b9eb1ede110991d6fe
Jeremi Olczak <n00230483@iadt.ie>
2024-11-04 12:36:40 +0000

    Updating migration file schema (Reviews)

--- 2024_Performance_Hub/database/migrations/2024_11_04_123444_create_reviews_table.php
+++ 2024_Performance_Hub/database/migrations/2024_11_04_123444_create_reviews_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('performance_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->text('content');
+            $table->integer('rating')->unsigned();
             $table->timestamps();
         });
     }
