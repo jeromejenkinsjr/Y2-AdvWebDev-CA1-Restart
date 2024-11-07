0713f7cae78260c576e7ec7fee4ceaeb3cecc721
Jeremi Olczak <n00230483@iadt.ie>
2024-11-07 00:26:33 +0000

    Altering comment in web.php

--- 2024_Performance_Hub/routes/web.php
+++ 2024_Performance_Hub/routes/web.php
@@ -25,7 +25,7 @@ Route::get('/performances/create', [PerformanceController::class, 'create'])->na
 Route::get('/performances/{performance}', [PerformanceController::class, 'show'])->name('performances.show');
 Route::post('/performances', [PerformanceController::class, 'store'])->name('performances.store');
 
-// The routes below are for displaying the edit form for a performance. {performance} is a route parameter representing the ID of the performance to be edited. The 'edit' method in PerformanceController is responsible for fetching the specific performance and returning the edit form view.
+// The 2 routes below are for displaying the edit form for a performance. {performance} is a route parameter representing the ID of the performance to be edited. The 'edit' method in PerformanceController is responsible for fetching the specific performance and returning the edit form view.
 
 Route::get('/performances/{performance}/edit', [PerformanceController::class, 'edit'])->name('performances.edit');
 Route::put('/performances/{performance}', [PerformanceController::class, 'update'])->name('performances.update');
