a0e55b1bd8be8be64c9e248b2f2ce511351015dc
Jeremi Olczak <n00230483@iadt.ie>
2024-11-07 01:00:03 +0000

    Altering navigation and nav-link styles and adding a new background

--- 2024_Performance_Hub/resources/views/layouts/app.blade.php
+++ 2024_Performance_Hub/resources/views/layouts/app.blade.php
@@ -14,23 +14,25 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+    <body class="font-sans antialiased" style="background-image: url('{{ asset('images/bgmusic.jpg') }}'); background-size: cover; background-position: center;">
 
-            <!-- Page Heading -->
-            @isset($header)
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endisset
+    <div class="min-h-screen bg-opacity-70">
+        @include('layouts.navigation')
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
-    </body>
+        <!-- Page Heading -->
+        @isset($header)
+            <header class="bg-white shadow bg-opacity-80">
+                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    {{ $header }}
+                </div>
+            </header>
+        @endisset
+
+        <!-- Page Content -->
+        <main>
+            {{ $slot }}
+        </main>
+    </div>
+</body>
+
 </html>
--- 2024_Performance_Hub/resources/views/layouts/navigation.blade.php
+++ 2024_Performance_Hub/resources/views/layouts/navigation.blade.php
@@ -70,7 +70,7 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden bg-black">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" class="text-gray-300 hover:bg-gray-700 hover:text-white">
+            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" class="text-gray-700 hover:bg-gray-700 hover:text-gray-300">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
         </div>
