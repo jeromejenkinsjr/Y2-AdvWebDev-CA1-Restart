62f7a32e3466c3f37631bd02e0dd18e942e2f106
Jeremi Olczak <n00230483@iadt.ie>
2024-11-09 17:49:02 +0000

    Adjusting burger menu in navigation to add Performances tab

--- 2024_Performance_Hub/resources/views/layouts/navigation.blade.php
+++ 2024_Performance_Hub/resources/views/layouts/navigation.blade.php
@@ -69,11 +69,14 @@
 
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden bg-black">
-        <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" class="text-gray-700 hover:bg-gray-700 hover:text-gray-300">
-                {{ __('Dashboard') }}
-            </x-responsive-nav-link>
-        </div>
+    <div class="pt-2 pb-3 space-y-1">
+        <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" class="text-gray-700 hover:bg-gray-700 hover:text-gray-300">
+            {{ __('Dashboard') }}
+        </x-responsive-nav-link>
+        <x-responsive-nav-link :href="route('performances.index')" :active="request()->routeIs('performances.index')" class="text-gray-700 hover:bg-gray-700 hover:text-gray-300">
+            {{ __('Performances') }}
+        </x-responsive-nav-link>
+    </div>
 
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-700">
