3ccf1f59404c9b64676f497f7231c372eddbead3
Jeremi Olczak <n00230483@iadt.ie>
2024-11-09 12:16:03 +0000

    Adjusting name of migration file and adding relationships to UserView

--- 2024_Performance_Hub/app/Models/UserView.php
+++ 2024_Performance_Hub/app/Models/UserView.php
@@ -3,8 +3,26 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class UserView extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'performance_id',
+        'composer',
+    ];
+
+    // Define relationships
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function performance()
+    {
+        return $this->belongsTo(Performance::class);
+    }
 }
