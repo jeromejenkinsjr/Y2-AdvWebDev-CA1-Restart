4d2794da9126d6884adea7bc6be4e665f0a8899c
Jeremi Olczak <n00230483@iadt.ie>
2024-11-18 14:48:30 +0000

    Adding graceful error return instead of 403 Error

--- 2024_Performance_Hub/app/Http/Middleware/CheckAdmin.php
+++ 2024_Performance_Hub/app/Http/Middleware/CheckAdmin.php
@@ -14,6 +14,6 @@ class CheckAdmin
             return $next($request);
         }
 
-        abort(403, 'Unauthorized action.');
+        return to_route('performances.index')->with('error', 'Unauthorised access.');
     }
 }
