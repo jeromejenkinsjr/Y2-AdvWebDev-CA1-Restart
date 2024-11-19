ed4bcbd70af013f869d413c1ee7728a1d8a0985d
Jeremi Olczak <n00230483@iadt.ie>
2024-11-19 13:35:26 +0000

    Adjusting textarea to complete spaces in Review box fix

--- 2024_Performance_Hub/resources/views/components/review-form.blade.php
+++ 2024_Performance_Hub/resources/views/components/review-form.blade.php
@@ -22,8 +22,7 @@
     <!-- Review Content -->
     <div class="mb-4">
         <label for="content" class="block text-sm text-gray-700">Review</label>
-        <textarea name="content" id="content" required class="mt-1 block w-full border-gray-300 rounded-md shadow-sm">
-            {{ $review ? $review->content : '' }}</textarea>
+        <textarea name="content" id="content" required class="mt-1 block w-full border-gray-300 rounded-md shadow-sm">{{ $review ? $review->content : '' }}</textarea>
         @error('content')
             <p class="text-sm text-red-600">{{ $message }}</p>
         @enderror
