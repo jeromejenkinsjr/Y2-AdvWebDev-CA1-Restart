9ba37b3977ea5258e6b4807ea1ce31fd0e7a1579
Jeremi Olczak <n00230483@iadt.ie>
2024-12-02 12:42:16 +0000

    Creating tags model and migration file

---
+++ 2024_Performance_Hub/database/migrations/2024_12_02_120602_create_tags_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tags', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->timestamps();
+        });
+
+        // Pivot table for performance_tag
+        Schema::create('performance_tag', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('performance_id')->constrained()->onDelete('cascade');
+            $table->foreignId('tag_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('performance_tag');
+        Schema::dropIfExists('tags');
+    }
+};
